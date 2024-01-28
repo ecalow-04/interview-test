59efb1784b453d2df4a2bacddd9edb11b6c6d60e
Ellis Calow <milburnthom@gmail.com>
2024-01-28 22:51:01 +0000

    add: profile page display info

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,33 +4,52 @@ import styles from './styles.module.css';
 import { useAuth } from '../../utils/useAuth';
 import { useEffect, useState } from "react";
 
-interface UserData {
+interface Profile {
     username: string;
     name: string;
-    age: number;
+    email: string;
 }
 
 export default function Profile() {
-    const { authToken, logout } = useAuth();
-    const [userData, setUserData] = useState<UserData>()
+    const { isLoggedIn, authToken, logout } = useAuth();
+    const [profileLoading, setProfileLoading] = useState<boolean>(false);
+    const [profile, setProfile] = useState<Profile>();
 
-    // useEffect(() => {
-    //     if (!userData) {
-    //         fetch('/api/profile', {
-    //             method: 'GET',
-    //             headers: {
-    //                 'Authorization': `Bearer ${authToken}`
-    //             }
-    //         }).then((res) => {
-    //             console.log('this is res', res.json())
-    //             setUserData({ username: 'test', name: 'test', age: 20 })
-    //         });
-    //     }
-    // })
+    useEffect(() => {
+        if (isLoggedIn && !profile && !profileLoading) {
+            setProfileLoading(true);
+            requestProfile();
+        }
+    });
+
+    const requestProfile = async () => {
+        const response = await fetch('/api/profile', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${authToken}`
+            }
+        });
+        
+        // logout as profile associated to token not found
+        if (response.status !== 200) {
+            logout();
+            return;
+        };
+
+        const data = await response.json();
+
+        setProfile(data);
+    }
 
     return (
-        <main>
-            <div>hi</div>
+        <main className={styles.main}>
+            {profile && (
+                <div className={styles.container}>
+                    <h1 className={styles.h1}>Name: { profile.name }</h1>
+                    <h1 className={styles.h1}>Email: { profile.email }</h1>
+                    <button className={styles.logoutButton} onClick={logout}>Logout</button>
+                </div>
+            )}
             {/* <div id="login-container">
                 <h1 className="heading">Welcome</h1>
                 <p className="description">Please login below to continue</p>
