7124a1803824b6697cbf88726855cda75a349848
Ellis Calow <milburnthom@gmail.com>
2024-01-28 13:09:48 +0000

    change: use auth hook on login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,44 @@
 'use client';
 
-import { FormEvent } from 'react'
+import { FormEvent } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '../utils/useAuth';
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const { login, authToken } = useAuth();
+    const [error, setError] = useState<string | null>(null);
+    const [usernameInput, setUsernameInput] = useState<string>('');
+    const [passwordInput, setPasswordInput] = useState<string>('');
+    const router = useRouter();
+
+    useEffect(() => {
+        if (authToken) {
+            router.push('/profile');
+        }
+    });
+
     const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         const formData = {
             "username": event.currentTarget.username.value,
             "password": event.currentTarget.password.value
+        };
+
+        const loginStatus = await login(formData);
+
+        if (loginStatus == 200) {
+            router.push('/profile')
+        } else if (loginStatus == 401) {
+            // unauthorized
+            setError('Incorrect username or password');
+        } else if (loginStatus == 400) {
+            // bad request
         }
-
-        const response = await fetch('/api/login', {
-            method: 'POST',
-            body: JSON.stringify(formData),
-        })
-
-        const data = await response.json()
-
-        console.log(data, 'this is data');
     };
+
+    const shouldDisableSubmit = usernameInput.length === 0 || passwordInput.length === 0;
 
     return (
         <main>
@@ -27,9 +46,32 @@ export default function Home() {
                 <h1 className="heading">Welcome</h1>
                 <p className="description">Please login below to continue</p>
                 <form id="login-form" onSubmit={onSubmit}>
-                    <input id="username" name="username" type="text" placeholder="Username" />
-                    <input id="password" name="password" type="password" placeholder="Password" />
-                    <button type="submit">Login</button>
+                    {error && (
+                        <div className="error-message">{ error }</div>
+                    )}
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Username"
+                        className={error ? 'border-error' : ''}
+                        onChange={(e) => {
+                            setError(null);
+                            setUsernameInput(e.target.value);
+                        }}
+                    />
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        className={error ? 'border-error' : ''}
+                        onChange={(e) => {
+                            setError(null);
+                            setPasswordInput(e.target.value);
+                        }}
+                    />
+                    <button type="submit" disabled={shouldDisableSubmit}>Login</button>
                 </form>
             </div>
         </main>
