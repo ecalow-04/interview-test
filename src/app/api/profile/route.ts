86b394f128113fe5e07a479cd2d60331159d9480
Ellis Calow <milburnthom@gmail.com>
2024-01-28 22:48:45 +0000

    add: logic for getting profile from token

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,1 +1,25 @@
-/** @TODO */
+interface Profile {
+    username: string;
+    name: string;
+    email: string;
+    token?: string;
+}
+
+const profiles: Array<Profile> = [
+    { username: 'admin', name: 'Walter White', email: 'walterhwhite@gmail.com', token: '123' }
+]
+
+export async function GET(request: Request) {
+    const authToken = request.headers?.get('Authorization')?.replace('Bearer ', '')
+    if (!authToken) return new Response("Unauthorized", { status: 401 });
+    
+    const profile = profiles.find(p => p.token === authToken)
+
+    if (profile) {
+        const data = { ...profile };
+        delete data.token;
+        return new Response(JSON.stringify(data), { status: 200 });
+    } else {
+        return new Response('Not Found', { status: 404 });
+    }
+}
