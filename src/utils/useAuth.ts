002720053b0ee2b2853cb1a81065f974f733bdbb
Ellis Calow <milburnthom@gmail.com>
2024-01-28 13:08:12 +0000

    add: useAuth hook

---
+++ src/utils/useAuth.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/navigation';
+
+interface LoginDetails {
+    username: string;
+    password: string;
+}
+
+export const useAuth = () => {
+    const router = useRouter();
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [authToken, setAuthToken] = useState<string | null>(null);
+
+    useEffect(() => {
+        const token = window.localStorage.getItem('token');
+        if (!authToken) {
+            if (token && validateToken(token)) {
+                setAuthToken(token);
+                setIsLoggedIn(true);
+            } else {
+                logout();
+            }
+        }
+    });
+
+    const validateToken = (token: string) => {
+        if (token.length == 3) return true
+        return false
+    }
+
+    const login = async (loginDetails: LoginDetails) => {
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            body: JSON.stringify(loginDetails),
+        });
+
+        if (response.status == 200) {
+            const data = await response.json();
+
+            window.localStorage.setItem('token', data.token)
+        }
+
+        return response.status;
+    }
+
+    const logout = () => {
+        window.localStorage.removeItem('token');
+        router.push('/');
+    }
+
+    return { login, logout, authToken }
+}
