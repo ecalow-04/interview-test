30bb7dd88ccbc5fc3fe9fcedc29f36d98d966115
Ellis Calow <milburnthom@gmail.com>
2024-01-29 15:50:18 +0000

    remove: old code

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -52,15 +52,6 @@ export default function Profile() {
                     </button>
                 </div>
             )}
-            {/* <div id="login-container">
-                <h1 className="heading">Welcome</h1>
-                <p className="description">Please login below to continue</p>
-                <form id="login-form" onSubmit={onSubmit}>
-                    <input id="username" name="username" type="text" placeholder="Username" />
-                    <input id="password" name="password" type="password" placeholder="Password" />
-                    <button type="submit">Login</button>
-                </form>
-            </div> */}
         </main>
     );
 }
